d55b819a287ebbd356aee047ac2af7d3dcfc31da
sulaymonn07 <i02245129@gmail.com>
2025-01-29 12:14:09 +0000

    first commit

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -4,7 +4,8 @@ const TodoItem =(props) => {
     return(
         <>
         <div className="todo">
-            <span> {props.title}</span>
+            <span> {props.id}. {props.title}</span>
+            <i onClick={() => {alert(props.id)}} class="fa-solid fa-trash"></i>
           </div>
         </>
     )
--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -2,31 +2,42 @@ import { useState } from "react";
 import TodoItem from "./TodoItem";
 
 const Todo = () => {
+  const [value, setValue] = useState("  ")
   const [todos, setTodos] = useState([
-    { title: "Homework" },
-    { title: "Go to School" },
-  ]);
+    { 
+      id: 1, 
+       title: "Homework"
+    },
+  ]);  
 
-  console.log(todos);
+  // console.log(todos);
+
+  const onClick =() => {
+    const newTodos = [...todos,{ id: todos.length+1  ,title: value}]
+    console.log(newTodos);
+    setTodos(newTodos)
+    setValue("")
+    console.log(todos);
+    
+  }
 
   return (
     <>
       <div className="todo-container">
         <h2 className="app-title">Todo App</h2>
         <div className="todo-header">
-          <input
-            className="add-input"
-            type="text"
-            placeholder="Vazifa yozing.."
-          />
-          <button className="add-btn">Add</button>
+          <input value={value} onChange={(e) => setValue(e.target.value)} className="add-input" type="text" placeholder="Vazifa yozing.."/>
+
+          <button onClick={onClick} className="add-btn">Add</button>
+
+            <h1>{value}</h1>
         </div>
 
         <div className="todo-list"> 
           {
             todos.map(t => {
               return(
-                <TodoItem title={t.title}/>
+                <TodoItem id={t.id} title={t.title}/>
               )
             })
           }
